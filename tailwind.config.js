448bcc527e17c4b578fb75bc0717fad49d2ad885
mr-leo44 <lionnelkaniki44@gmail.com>
2024-06-19 01:20:35 +0200

    ajout de flowbite-typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,73 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
+    darkMode: "class",
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            colors: {
+                primary: {
+                    50: "#ecfdf5",
+                    100: "#d1fae5",
+                    200: "#a7f3d0",
+                    300: "#6ee7b7",
+                    400: "#34d399",
+                    500: "#10b981",
+                    600: "#059669",
+                    700: "#047857",
+                    800: "#065f46",
+                    900: "#064e3b",
+                    950: "#022c22",
+                },
             },
         },
+        fontFamily: {
+            body: [
+                "Montserrat",
+                "ui-sans-serif",
+                "system-ui",
+                "-apple-system",
+                "system-ui",
+                "Segoe UI",
+                "Roboto",
+                "Helvetica Neue",
+                "Arial",
+                "Noto Sans",
+                "sans-serif",
+                "Apple Color Emoji",
+                "Segoe UI Emoji",
+                "Segoe UI Symbol",
+                "Noto Color Emoji",
+            ],
+            sans: [
+                "Montserrat",
+                "ui-sans-serif",
+                "system-ui",
+                "-apple-system",
+                "system-ui",
+                "Segoe UI",
+                "Roboto",
+                "Helvetica Neue",
+                "Arial",
+                "Noto Sans",
+                "sans-serif",
+                "Apple Color Emoji",
+                "Segoe UI Emoji",
+                "Segoe UI Symbol",
+                "Noto Color Emoji",
+            ],
+        },
     },
-
-    plugins: [forms],
+    plugins: [
+        forms,
+        require("flowbite-typography"),
+        require("flowbite/plugin"),
+    ],
 };
