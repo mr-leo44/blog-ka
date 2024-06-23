917bd82e1b53f614d024d3efe988d7e7fa1696f2
mr-leo44 <lionnelkaniki44@gmail.com>
2024-06-23 09:39:43 +0200

    définition de la RouteKeyName en ajoutant le slug dan la migration des categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Article;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -14,6 +15,22 @@ class Category extends Model
         'name',
         'cover_img',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::saving(function($model){
+            if(empty($model->slug)){
+                $randomNumber = rand(100, 9999999);
+                $model->slug = Str::slug($model->name). '-'. $randomNumber;
+            }
+        });
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     public function articles (): HasMany
     {
