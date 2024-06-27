1235c28a2e9b25c48d7d78150ae00cf9559ceaf9
mr-leo44 <lionnelkaniki44@gmail.com>
2024-06-27 09:25:02 +0200

    création d'un post

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
-use App\Http\Requests\StoreArticleRequest;
-use App\Http\Requests\UpdateArticleRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+// use App\Http\Requests\StoreArticleRequest;
+// use App\Http\Requests\UpdateArticleRequest;
 
 class ArticleController extends Controller
 {
@@ -13,7 +18,9 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Article::with(['category', 'user'])->paginate(10);
+        $categories = Category::all();
+        return view('posts.index', compact('categories', 'posts'));
     }
 
     /**
@@ -27,9 +34,20 @@ class ArticleController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreArticleRequest $request)
+    public function store(Request $request)
     {
-        //
+        if ($request->file('cover_img')){
+            $cover = $request->file('cover_img')->store('posts/covers');
+        }
+        Article::create([
+            'title' => $request->title, 
+            'content' => $request->content, 
+            'category_id' => $request->category_id, 
+            'user_id' => Auth::user()->id, 
+            'cover_img' => $cover ?? null
+        ]);
+
+        return redirect()->route('posts.index')->with('success', 'La catégorie a été créée avec succès');
     }
 
     /**
@@ -51,9 +69,20 @@ class ArticleController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateArticleRequest $request, Article $article)
+    public function update(Request $request, Article $article)
     {
-        //
+        if ($request->hasFile('cover_img')){
+            Storage::delete($article->cover_img);
+            $cover = $request->file('cover_img')->store('posts/covers');
+        }
+
+        $article->update([
+            'title' => $request->title,
+            'content' => $request->content, 
+            'category_id' => $request->category_id,
+            'cover_img' => $cover ?? $article->cover_img
+        ]);
+        return redirect()->route('posts.index')->with('success', 'L\'article a été mis à jour avec succès');
     }
 
     /**
@@ -61,6 +90,10 @@ class ArticleController extends Controller
      */
     public function destroy(Article $article)
     {
-        //
+        if ($article->cover_img){
+            Storage::delete($article->cover_img);
+        }
+        $article->delete();
+        return redirect()->route('posts.index')->with("success","suppression de l\article reussie");
     }
 }
