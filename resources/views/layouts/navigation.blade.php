3e08fcfa2df441702a23f9d6a3b6804ed01ba41e
mr-leo44 <lionnelkaniki44@gmail.com>
2024-06-29 10:54:32 +0200

    fix de la couleur d'un link dans la navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,7 +20,7 @@
                 class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
                     <a href="{{ route('dashboard') }}"
-                        class="{{ request()->routeIs('dashboard') ? 'text-emerald-500 dark:text-emerald-500 md:dark:text-emerald-500' : 'text-gray-900 dark:text-white md:dark:text-white' }} block py-2 px-3 rounded md:bg-transparent md:text-emerald-700 md:p-0 md:dark:hover:text-emerald-500"
+                        class="{{ request()->routeIs('dashboard') ? 'text-emerald-500 dark:text-emerald-500 md:dark:text-emerald-500' : 'text-gray-900 dark:text-white md:dark:text-white' }} block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-emerald-700 md:p-0  md:dark:hover:text-emerald-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                         aria-current="page">Dashboard</a>
                 </li>
                 <li>
