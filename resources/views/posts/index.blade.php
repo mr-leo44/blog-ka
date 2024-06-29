cc6423ea5c76f9ce508dee589b888c85dcf67ca1
mr-leo44 <lionnelkaniki44@gmail.com>
2024-06-29 10:54:15 +0200

    Ajout du bouton show dans index d'un post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -85,18 +85,28 @@
                                         {{ $post->title }}
                                     </td>
                                     <td class="px-6 py-3">
-                                        {{ $post->category->name }}
-                                    </td>
-                                    <td class="px-6 py-3">
-                                        {{ $post->content }}
+                                        {{ Str::limit($post->content, 20, '...') }}
                                     </td>
                                     <td class="px-6 py-3">
                                         {{ $post->user->name }}
                                     </td>
                                     <td class="px-6 py-3">
-                                        {{ $post->is_published }}
+                                        {{ $post->category->name }}
+                                    </td>
+                                    <td class="px-6 py-3">
+                                        {{ $post->created_at->format('d-m-Y') }}
                                     </td>
                                     <td class="px-6 py-3 flex justify-end">
+                                        <a href="{{ route('posts.show', $post) }}">
+                                            <div class="font-medium cursor-pointer bg-amber-400 hover:bg-amber-400 dark:bg-amber-400 py-2 px-3 rounded me-3 text-white dark:text-white">
+                                                <svg class="w-5 h-5 text-white dark:text-white"
+                                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path fill-rule="evenodd"
+                                                        d="M4.998 7.78C6.729 6.345 9.198 5 12 5c2.802 0 5.27 1.345 7.002 2.78a12.713 12.713 0 0 1 2.096 2.183c.253.344.465.682.618.997.14.286.284.658.284 1.04s-.145.754-.284 1.04a6.6 6.6 0 0 1-.618.997 12.712 12.712 0 0 1-2.096 2.183C17.271 17.655 14.802 19 12 19c-2.802 0-5.27-1.345-7.002-2.78a12.712 12.712 0 0 1-2.096-2.183 6.6 6.6 0 0 1-.618-.997C2.144 12.754 2 12.382 2 12s.145-.754.284-1.04c.153-.315.365-.653.618-.997A12.714 12.714 0 0 1 4.998 7.78ZM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                                                        clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+                                        </a>
                                         <a data-modal-target="edit-modal" data-modal-toggle="edit-modal"
                                             class="font-medium cursor-pointer bg-blue-600 hover:bg-blue-700 dark:bg-blue-700 py-2 px-3 rounded me-3 text-white dark:text-white">
                                             <svg class="w-5 h-5 text-white dark:text-white" aria-hidden="true"
@@ -119,7 +129,7 @@
                                         </a>
                                     </td>
                                 </tr>
-                                <x-post-edit :categories="$categories" :post="$post" />
+                                {{-- <x-post-edit :categories="$categories" :post="$post" /> --}}
                             @endforeach
                         @else
                             <tr>
