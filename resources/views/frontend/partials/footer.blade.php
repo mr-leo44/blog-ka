38595fb01f0bb315fc4b0bbb38ed33f9674affc2
mr-leo44 <lionnelkaniki44@gmail.com>
2024-07-03 09:12:48 +0200

    fix des petits details du layout de frontend

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -15,6 +15,6 @@
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="/"
-                class="hover:underline">Mukasa-Kasima Archikin™</a>. Tous les droits Reservés.</span>
+                class="hover:underline">Mukasa-Kasima Archikin</a>. Tous les droits Reservés.</span>
     </div>
 </footer>
