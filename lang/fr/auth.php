056daa9706cc842edbd2b739b1276b69181ceb7f
mr-leo44 <lionnelkaniki44@gmail.com>
2024-07-06 16:41:37 +0100

    implementation du module en français

---
+++ lang/fr/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Les données saisies sont incorrectes.',
+    'password' => 'Le mot de passe fourni est incorrecte.',
+    'throttle' => 'Ressayez s\'il vous plait dans :seconds secondes.',
+
+];
---
+++ lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a bien été reinitialisé.',
+    'sent' => 'Nous vous avons envoyé un mail avec le lien de réinitialisation.',
+    'throttled' => 'Patientez un instant avant de ressayer.',
+    'token' => 'Ce lien de réinitialisation est invalide.',
+    'user' => "Aucun compte trouvé avec cet email.",
+
+];
