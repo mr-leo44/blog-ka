7c6be54009e3106807674916ea1963f074977371
mr-leo44 <lionnelkaniki44@gmail.com>
2024-07-18 08:40:59 +0100

    ajout du lien to dashboard ppour l'user connected

--- resources/views/frontend/partials/navigation.blade.php
+++ resources/views/frontend/partials/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="bg-emerald-700 dark:bg-gray-900 shadow">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-        <a href="{{ route("welcome") }}" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="{{ route('welcome') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="{{ asset('images/logo.png') }}" class="self-center text-white h-8" alt="Logo" />
             <span
                 class="self-center text-md md:text-xl font-semibold md:font-semibold whitespace-nowrap text-white dark:hover:text-emerald-400">Mukasa-Kasima
@@ -9,7 +9,9 @@
         <div class="flex items-center space-x-6 rtl:space-x-reverse">
             <a href="tel:+243899939371" class="text-xs md:text-sm text-white hover:underline">(+243)
                 899-939-371</a>
-            {{-- <a href="#" class="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a> --}}
+            @if (Auth::user())
+                <a href="{{route('dashboard') }}" class="text-sm text-gray-900 dark:text-white hover:underline">Dashboard</a>
+            @endif
         </div>
     </div>
 </nav>
