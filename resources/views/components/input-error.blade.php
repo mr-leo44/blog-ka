866dad7c11061db02403e9f9a555c99825274de8
mr-leo44 <lionnelkaniki44@gmail.com>
2024-07-22 18:06:19 +0100

    update de error input

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -3,7 +3,7 @@
 @if ($messages)
     <ul {{ $attributes->merge(['class' => 'text-xs text-red-600 space-y-1']) }}>
         @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+            <li class="list-none">{{ $message }}</li>
         @endforeach
     </ul>
 @endif
