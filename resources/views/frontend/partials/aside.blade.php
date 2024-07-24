78cd408e2585ba4d8c5c55786eb46035a883524b
mr-leo44 <lionnelkaniki44@gmail.com>
2024-07-24 07:54:54 +0100

    petite modif

--- resources/views/frontend/partials/aside.blade.php
+++ resources/views/frontend/partials/aside.blade.php
@@ -1,6 +1,6 @@
 <div class="mx-3 px-2 my-8 md:my-0">
     <div class="mb-4">
-        <h5 class="mb-2 border-b-2"><span class="text-xs md:text-sm font-medium tracking-tight text-gray-900 text-white bg-emerald-700 px-2 py-1">Catégories</span></h5>
+        <h5 class="mb-2 border-b-2"><span class="text-xs md:text-sm font-medium tracking-tight text-gray-900 dark:text-white bg-emerald-700 px-2 py-1">Catégories</span></h5>
         <ul class="max-w-md ms-2 space-y-2 text-gray-500 list-disc list-inside dark:text-gray-400">
             @if ($categories->count() > 0)
                 @foreach ($categories as $category)
@@ -17,22 +17,13 @@
                 </li>
             @endif
         </ul>
-        {{-- <a href="{{ route('getCategories') }}"
-            class="inline-flex items-center mb-4 pt-2 text-xs text-center text-emerald-700 rounded-lg hover:text-emerald-600">
-            Plus de catégories
-            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9" />
-            </svg>
-        </a> --}}
     </div>
     <div class="mt-6">
-        <h5 class="mb-2 border-b-2"><span class="text-xs md:text-sm font-medium tracking-tight text-gray-900 text-white bg-emerald-700 px-2 py-1">Auteurs</span></h5>
+        <h5 class="mb-2 border-b-2"><span class="text-xs md:text-sm font-medium tracking-tight text-gray-900 dark:text-white bg-emerald-700 px-2 py-1">Auteurs</span></h5>
         <ul class="max-w-md ms-2 space-y-2 text-gray-500 list-disc list-inside dark:text-gray-400">
             @if ($authors->count() > 0)
                 @foreach ($authors as $user)
-                    <li class="font-normal text-xs md:text-sm text-gray-800 dark:text-white hover:text-emerald-700">
+                    <li class="list-none font-normal text-xs md:text-sm text-gray-800 dark:text-white hover:text-emerald-700">
                         <a href="{{ route('authorPosts', $user) }}">
                             {{ $user->name }}
                         </a>
@@ -44,15 +35,6 @@
                 </li>
             @endif
         </ul>
-        {{-- <a href="{{ route('getAuthors') }}"
-        class="inline-flex items-center mb-4 pt-2 text-xs text-center text-emerald-700 rounded-lg hover:text-emerald-600">
-            Voir plus
-            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9" />
-            </svg>
-        </a> --}}
     </div>
     <hr class="h-1 mt-12 bg-gray-100 border-0 rounded dark:bg-gray-700">
     <div class="my-4">
--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-emerald-700 dark:bg-gray-900 shadow bottom-0 shadow mt-4">
+<footer class="bg-emerald-700 dark:bg-gray-900 shadow bottom-0 mt-4">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="{{ route('welcome') }}" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
