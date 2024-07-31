9dbe3a7ddad716d0136128d17e5c589a98944d0a
mr-leo44 <lionnelkaniki44@gmail.com>
2024-07-31 14:34:45 +0100

    update guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,9 +16,9 @@
 
 <body class="font-sans text-gray-900 antialiased">
     <div class="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-800">
-        <div>
+        <div class="my-8">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-application-logo class="w-20 h-20 text-gray-500" />
             </a>
         </div>
 
