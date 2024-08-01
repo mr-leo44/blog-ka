547c63609d0a42d00ceadd41bf9b3b20dddb67c4
mr-leo44 <lionnelkaniki44@gmail.com>
2024-08-01 14:19:57 +0100

    ajou de la key pour le route name

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Article;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -12,8 +13,31 @@ class Tag extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name'
+        'name',
+        'slug'
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::saving(function ($model) {
+            if (empty($model->slug)) {
+                $randomNumber = rand(100, 10000);
+                $model->slug = Str::slug($model->name) . '-' . $randomNumber;
+            }
+        });
+        static::updating(function ($model) {
+            if ($model->isDirty('name')) {
+                $randomNumber = rand(100, 9999999);
+                $model->slug = Str::slug($model->name) . '-' . $randomNumber;
+            }
+        });
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     public function articles(): BelongsToMany
     {
